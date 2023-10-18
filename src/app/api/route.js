2f02fc61e8dad09f2617806aa169876e92fbe2c9
BigSzy <szymon.kaluza@outlook.com>
2023-10-18 09:30:38 +0100

    start of contact form functionality

---
+++ src/app/api/route.js
@@ -0,0 +1,85 @@
+import { NextResponse } from "next/server";
+import { SendEmailCommand } from "@aws-sdk/client-ses";
+import { SESClient } from "@aws-sdk/client-ses";
+
+// Create SES service object.
+const sesClient = new SESClient({
+  region: process.env.REGION,
+  credentials: {
+    accessKeyId: process.env.KEY,
+    secretAccessKey: process.env.SECRET,
+  },
+});
+
+const createSendEmailCommand = (toAddress, fromAddress, body) => {
+  return new SendEmailCommand({
+    Destination: {
+      /* required */
+      CcAddresses: [
+        /* more items */
+      ],
+      ToAddresses: [
+        toAddress,
+        /* more To-email addresses */
+      ],
+    },
+    Message: {
+      /* required */
+      Body: {
+        /* required */
+        // Html: {
+        //   Charset: "UTF-8",
+        //   Data: "HTML_FORMAT_BODY",
+        // },
+        Text: {
+          Charset: "UTF-8",
+          Data: `
+          ${body.firstName} ${body.lastName}
+          ${body.mobile} 
+          ${body.email}
+          ${body.message}
+          `,
+        },
+      },
+      Subject: {
+        Charset: "UTF-8",
+        Data: body.subject,
+      },
+    },
+    Source: fromAddress,
+    ReplyToAddresses: [
+      /* more items */
+    ],
+  });
+};
+
+export async function POST(request) {
+  const body = await request.json();
+
+  const sendEmailCommand = createSendEmailCommand(
+    "szymon.kaluza@outlook.com",
+    "szymon.kaluza@outlook.com",
+    body
+  );
+
+  try {
+    console.log(body);
+
+    const racaptchaResponse = await fetch(
+      `https://www.google.com/recaptcha/api/siteverify?secret=${process.env.GOOGLE_SECRET_KEY}&response=${body.token}`,
+      { method: "POST" }
+    );
+    const recaptchaPayload = await racaptchaResponse.json();
+
+    if (recaptchaPayload.success) {
+      console.log("Recaptcha successful")
+      const response = await sesClient.send(sendEmailCommand);
+    }
+    const data = { message: "Hello world" };
+    return NextResponse.json(data);
+  } catch (e) {
+    console.error("Failed to send email.");
+    console.log(e);
+    return e;
+  }
+}
--- src/app/contact/ContactSection.jsx
+++ src/app/contact/ContactSection.jsx
@@ -1,3 +1,5 @@
+"use client";
+
 import styles from "./ContactSection.module.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
@@ -8,11 +10,52 @@ import {
 import Link from "next/link";
 import { Playfair_Display } from "next/font/google";
 import { Overpass } from "next/font/google";
+import { useRef } from "react";
+import ReCAPTCHA from "react-google-recaptcha";
 const overpass = Overpass({ subsets: ["latin"] });
 
 const playfairDisplay = Playfair_Display({ subsets: ["latin"] });
 
 export default function ContactSection() {
+  const firstName = useRef();
+  const lastName = useRef();
+  const email = useRef();
+  const mobile = useRef();
+  const subject = useRef();
+  const message = useRef();
+
+  async function submit(event) {
+
+    console.log("Submitting...")
+
+    const token  = await reRef.current.getValue()
+
+    const body = {
+      firstName: firstName.current.value,
+      lastName: lastName.current.value,
+      email: email.current.value,
+      mobile: mobile.current.value,
+      message: message.current.value,
+      subject: subject.current.value,
+      token,
+    };
+
+    console.log(body)
+
+    const response = await fetch("/api/contact", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(body),
+    });
+
+    const data = await response.json();
+
+    console.log(data);
+  }
+
+
   return (
     <section>
       <div className="container">
@@ -21,7 +64,7 @@ export default function ContactSection() {
             <h2 className={playfairDisplay.className}>My Contact Details.</h2>
             <h3>Szymon Kaluza</h3>
             <p>info@skwd.com</p>
-            <p>07494076862</p>
+            <p>07495067985</p>
             <div className={styles.social}>
               <div className={styles.icon}>
                 <Link href={"/"}>
@@ -49,18 +92,20 @@ export default function ContactSection() {
             <div className={styles.name}>
               <div className={styles.firstName}>
                 <span>First Name</span>
-                <input type="text" />
+                <input type="text" ref={firstName} />
               </div>
+
               <div className={styles.lastName}>
                 <span>Last Name</span>
-                <input type="text" />
+                <input type="text" ref={lastName} />
               </div>
             </div>
+
             <div className={styles.email}>
               <p>
                 Email Address <span>(required)</span>
               </p>
-              <input type="text" />
+              <input type="text" ref={email} />
             </div>
 
             <div className={styles.phone}>
@@ -70,7 +115,14 @@ export default function ContactSection() {
                   Not required but makes it easier sometimes if i can call you
                 </span>
               </p>
-              <input type="text" />
+              <input type="text" ref={mobile} />
+            </div>
+
+            <div className={styles.email}>
+              <p>
+                Subject <span>(required)</span>
+              </p>
+              <input type="text"  ref={subject}/>
             </div>
 
             <div className={styles.message}>
@@ -80,11 +132,12 @@ export default function ContactSection() {
               <textarea
                 placeholder="Send me all your questions "
                 className={overpass.className}
+                ref={message}
               ></textarea>
             </div>
 
             <div className={styles.button}>
-              <button>Submit</button>
+              <input type="submit"  className={overpass.className}/>
             </div>
           </div>
         </div>
